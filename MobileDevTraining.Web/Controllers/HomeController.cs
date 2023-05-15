cee00af6bfa5344c8b45203309a73de61f941fb5
Ardyanto Konstantin <ardy.konstantin02@gmail.com>
2023-05-15 14:46:57 +0700

    Add project files.

---
+++ MobileDevTraining.Web/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MobileDevTraining.Web.Models;
+
+namespace MobileDevTraining.Web.Controllers
+{
+    [Route("api/v1/home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private TimeSpan JakartaTimeZoneSpan = new TimeSpan(7, 0, 0);
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var news = new List<HomeNewsModel>
+            {
+                new HomeNewsModel
+                {
+                    HomeNewsId = 1,
+                    Title = "Kurikulum mobile app development dengan react native telah diselenggarakan",
+                    Timestamp = new DateTimeOffset(2023, 5, 15, 0, 0, 0, JakartaTimeZoneSpan)
+                },
+                new HomeNewsModel
+                {
+                    HomeNewsId = 1,
+                    Title = "Survey training 3 + 1",
+                    Timestamp = new DateTimeOffset(2023, 5, 15, 0, 0, 0, JakartaTimeZoneSpan)
+                }
+            };
+            return Ok(news);
+        }
+    }
+}
---
+++ MobileDevTraining.Web/Models/HomeNewsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MobileDevTraining.Web.Models
+{
+    public class HomeNewsModel
+    {
+        public int HomeNewsId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTimeOffset Timestamp { get; set; }
+    }
+}
